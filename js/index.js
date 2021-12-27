a61b2ff5e4e1490ceb6340f5b9e4575cf1a2ddaf
MReinbacherSurface <marcel_reinbacher@yahoo.de>
2021-12-27 15:41:35 +0100

    added option to select tables

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
+
 //  do things on load
 document.addEventListener("DOMContentLoaded", function () {
     sliderValueOnInput();
+    
+    // Elements
+    const tables = document.getElementsByClassName("table");
+
+    // Events
+    document.getElementById("personNumber").oninput = sliderValueOnInput;
+    for (let item of tables) {
+        item.onclick = onClickHandlerTable;
+    }
+
 });
-
-// Events
-document.getElementById("personNumber").oninput = sliderValueOnInput;
-
 
 // Event functions
 function sliderValueOnInput() {
@@ -13,3 +20,17 @@ function sliderValueOnInput() {
     var personNumberValue = document.getElementById("personNumberValue");
     personNumberValue.innerHTML = personNumberRange.value;
 };
+
+function onClickHandlerTable(event) {
+    const target = event.currentTarget;
+    if (target.classList.contains("not-available")) return false; // if not-available -> do nothing
+    if (target.classList.contains("selected")) { // deselect if selected
+        target.classList.remove("selected");
+    } else { // select if not selected
+        var selectedTables = document.querySelectorAll(".table.selected");
+        for (let item of selectedTables) {
+            item.classList.remove("selected");
+        }
+        target.classList.add("selected");
+    }
+}
